fb5e102237905c686fbf90efe22074ac1fad7257
Ariannie Monrroe <ariannie.monrroe@gmail.com>
2023-05-11 11:57:52 -0400

    API

--- JS/apiRest.js
+++ JS/apiRest.js
@@ -1,9 +1,25 @@
-function api(){
+function clima(latitud, longitud) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat="+ latitud + "&lon=" + longitud+ "&appid=da782411f0dd3e140c85a1c83494aa97")
+    .then(response =>  response.json())
+    .then(data => {
+        console.log(data);
+        let ubicacion = document.getElementById("ubicacion");
+        ubicacion.innerHTML += "Ubicacion: " + data.name;
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={da782411f0dd3e140c85a1c83494aa97}')
-        .then(response => response.json())
-        .then(data => {
-            // console.log(data)
-        
-        })
+        let temperatura = document.getElementById("temperatura");
+        let kelvin = 273.15;
+        let celsius= data.main.temp - kelvin;
+        temperatura.innerHTML = "Temperatura: " + Math.trunc(celsius) + "°"
+    })
+}
+
+function ubi() {
+    navigator.geolocation.getCurrentPosition(function(position){
+
+        let latitud = position.coords.latitude;
+        let longitud = position.coords.longitude;
+
+        console.log(latitud, longitud);
+        clima(latitud, longitud);
+    })
 }
